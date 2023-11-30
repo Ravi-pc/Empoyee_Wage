6cde016850ba35a8e1f3d6b0011b65f5c3db95c6
“Ravi-pc” <ravisingh98138@gmail.com>
2023-11-30 10:49:23 +0530

    [Ravi] Refactor: Make new changes to the monthly_wage function

--- emp.py
+++ emp.py
@@ -12,35 +12,79 @@
 
 import random
 
+wage_per_hour = 20
+day_per_month = 20
 
-def daily_wage():
+
+def monthly_wage(hour_per_day):
+    """
+
+              Description: monthly_wage function to calculate the monthly wage of
+               an employee.
+
+              Parameter: Working hours
+
+              Return:
+          """
+    month_wage = day_per_month * hour_per_day * day_per_month
+    print('Wage per month of an employee is ', month_wage)
+
+
+def calculate_wage(hours):
+    """
+
+            Description: calculate_wage function to calculate the wage of an employee
+                            working part-time or full-time.
+
+            Parameter: Working hours
+
+            Return:
+        """
+    total_wage = wage_per_hour * hours
+    print('Total wage of the Employee is ', total_wage)
+
+
+def emp_day():
     """
             Description: daily_wage function that is used to calculate the wage of an employee
                          part-time and full-time.
 
             Parameter: None
 
-            Return:
+            Return: 0 or 1
 
          """
-    emp = random.randint(0, 2)
-    wage_per_hour = 20
-    working_hour = 0
-    match emp:
-        case 0:
-            print('Employee is absent')
-            working_hour = 0
-        case 1:
-            print('Employee is present')
-            working_hour = 8
-        case 2:
-            print('Employee is working part-time')
-            working_hour = 4
+    atd = check_attendance()
+    if atd == 1:
+        print('Employee is present ')
+        day = check_attendance()
+        if day == 1:
+            print('Employee is working full-time ')
+            calculate_wage(8)
+            monthly_wage(8)
+        else:
+            print('Employee is working part-time ')
+            calculate_wage(4)
+    else:
+        print('Employee is absent ')
 
-    total_wage = working_hour * wage_per_hour
-    print('Total wage for the day is ', total_wage)
+
+def check_attendance():
+    """
+
+        Description: Attendance function checks the attendance of employee is present or
+                        not using random function.
+
+        Parameter: None
+
+        Return: 0 or 1
+
+     """
+
+    check = (random.randint(0, 1))
+    return check
 
 
 if __name__ == "__main__":
     print("Welcome to the Employee Wage Computation Program on master branch ")
-    daily_wage()
+    emp_day()
